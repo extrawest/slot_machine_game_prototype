168ce1da3130153f22fecea170517e4d383ae16e
Roman Ovsepian <roman.ovsepian@extrawest.com>
2022-12-13 17:28:25 +0200

    Create slot machine prototype

--- lib/screens/slot_game_gameplay.dart
+++ lib/screens/slot_game_gameplay.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_slot_machine/slot_machine.dart';
 import 'package:slot_machine_game/widgets/common/background_gradient_scaffold.dart';
 
 class SlotGamePlayScreen extends StatefulWidget {
@@ -9,10 +12,77 @@ class SlotGamePlayScreen extends StatefulWidget {
 }
 
 class _SlotGamePlayScreenState extends State<SlotGamePlayScreen> {
+  late final SlotMachineController _slotMachineController;
+
   @override
   Widget build(BuildContext context) {
-    return const GradientBackgroundScaffold(
-      child: Text('gaga-gag'),
+    return GradientBackgroundScaffold(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: SlotMachine(
+              rollItems: const [
+                RollItem(index: 1, child: Icon(Icons.add)),
+                RollItem(index: 1, child: Icon(Icons.access_alarm_outlined)),
+                RollItem(index: 1, child: Icon(Icons.account_balance_rounded)),
+                RollItem(index: 1, child: Icon(Icons.ad_units_sharp)),
+              ],
+              onCreated: (controller) {
+                _slotMachineController = controller;
+              },
+              onFinished: (controller) {},
+            ),
+          ),
+          const SizedBox(height: 25),
+          Padding(
+            padding: const EdgeInsets.only(top: 16),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: 72,
+                  height: 44,
+                  child: TextButton(
+                      child: Text('STOP'),
+                      onPressed: () => _onSlotMachineStop(index: 0)),
+                ),
+                const SizedBox(width: 8),
+                SizedBox(
+                  width: 72,
+                  height: 44,
+                  child: TextButton(
+                      child: Text('STOP'),
+                      onPressed: () => _onSlotMachineStop(index: 1)),
+                ),
+                const SizedBox(width: 8),
+                SizedBox(
+                  width: 72,
+                  height: 44,
+                  child: TextButton(
+                      child: Text('STOP'),
+                      onPressed: () => _onSlotMachineStop(index: 2)),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(height: 25),
+          TextButton(
+            onPressed: () => _onSlotMachineStart(),
+            child: const Text('dsadsadsa'),
+          ),
+        ],
+      ),
     );
+  }
+
+  void _onSlotMachineStart() {
+    final index = Random().nextInt(20);
+    _slotMachineController.start(hitRollItemIndex: index < 5 ? index : null);
+  }
+
+  void _onSlotMachineStop({required int index}) {
+    _slotMachineController.stop(reelIndex: index);
   }
 }
