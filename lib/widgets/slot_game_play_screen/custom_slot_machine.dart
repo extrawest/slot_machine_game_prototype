d53c7c2486ae291a44aee064001a15347f36ec20
Roman Ovsepian <roman.ovsepian@extrawest.com>
2022-12-15 14:45:48 +0200

    add BlocConsumer to custom_slot_machine.dart

--- lib/widgets/slot_game_play_screen/custom_slot_machine.dart
+++ lib/widgets/slot_game_play_screen/custom_slot_machine.dart
@@ -22,85 +22,100 @@ class _CustomSlotMachineState extends State<CustomSlotMachine> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Transform.scale(
-          scale: 1.5,
-          child: Container(
-            margin: const EdgeInsets.all(40.0),
-            padding: const EdgeInsets.only(top: 20),
-            height: 205,
-            width: 280,
-            decoration: const BoxDecoration(
-              image: DecorationImage(image: AssetImage(boardIc), fit: BoxFit.cover),
-            ),
-            child: AbsorbPointer(
-              child: SlotMachine(
-                reelHeight: 100,
-                reelItemExtent: 40,
-                reelSpacing: 6,
-                rollItems: rollItems,
-                onCreated: (controller) {
-                  _slotMachineController = controller;
-                },
-                onFinished: (controller) {},
+    return BlocConsumer<SlotMachineCubit, SlotMachineState>(
+      listenWhen: (prev, curr) => prev.prizeIndex != curr.prizeIndex,
+      listener: _slotMachineListener,
+      builder: (context, state) => Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Transform.scale(
+            scale: 1.5,
+            child: Container(
+              margin: const EdgeInsets.all(40.0),
+              padding: const EdgeInsets.only(top: 20),
+              height: 205,
+              width: 280,
+              decoration: const BoxDecoration(
+                image: DecorationImage(image: AssetImage(boardIc), fit: BoxFit.cover),
+              ),
+              child: AbsorbPointer(
+                child: SlotMachine(
+                  reelHeight: 100,
+                  reelItemExtent: 40,
+                  reelSpacing: 6,
+                  rollItems: rollItems,
+                  onCreated: (controller) {
+                    _slotMachineController = controller;
+                  },
+                  onFinished: (controller) {},
+                ),
               ),
             ),
           ),
-        ),
-        const SizedBox(height: 25),
-        _buildStopButtons(),
-        const SizedBox(height: 25),
-        ZoomTapAnimation(
-          onTap: () => _onSlotMachineStart(),
-          child: SvgPicture.asset(playButton, width: 200),
-        ),
-      ],
-    );
-  }
-
-  Widget _buildStopButtons() {
-    return Padding(
-      padding: const EdgeInsets.only(top: 16),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          StopButton(
-            onPressed: () {
-              context.read<SlotMachineCubit>().setSlotsValue(firstSlot: false);
-              _onSlotMachineStop(index: 0);
-            },
-          ),
-          const SizedBox(width: 32),
-          StopButton(
-            onPressed: () {
-              context.read<SlotMachineCubit>().setSlotsValue(secondSlot: false);
-              _onSlotMachineStop(index: 1);
-            },
-          ),
-          const SizedBox(width: 32),
-          StopButton(
-            onPressed: () {
-              context.read<SlotMachineCubit>().setSlotsValue(thirdSlot: false);
-              _onSlotMachineStop(index: 2);
-            },
+          const SizedBox(height: 25),
+          _buildStopButtons(state),
+          const SizedBox(height: 25),
+          ZoomTapAnimation(
+            onTap: () => (state.isFirstSlotSpinning || state.isSecondSlotSpinning || state.isThirdSlotSpinning)
+                ? null
+                : _onSlotMachineStart(),
+            child: SvgPicture.asset(playButton, width: 200),
           ),
         ],
       ),
     );
   }
 
+  Widget _buildStopButtons(SlotMachineState state) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 16),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          StopButton(
+            onPressed: state.isFirstSlotSpinning
+                ? () {
+                    context.read<SlotMachineCubit>().setSlotsValue(firstSlot: false);
+                    _onSlotMachineStop(index: 0);
+                  }
+                : null,
+          ),
+          const SizedBox(width: 32),
+          StopButton(
+            onPressed: state.isSecondSlotSpinning
+                ? () {
+                    context.read<SlotMachineCubit>().setSlotsValue(secondSlot: false);
+                    _onSlotMachineStop(index: 1);
+                  }
+                : null,
+          ),
+          const SizedBox(width: 32),
+          StopButton(
+            onPressed: state.isThirdSlotSpinning
+                ? () {
+                    context.read<SlotMachineCubit>().setSlotsValue(thirdSlot: false);
+                    _onSlotMachineStop(index: 2);
+                  }
+                : null,
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _slotMachineListener(BuildContext context, SlotMachineState state) {}
+
   void _onSlotMachineStart() {
     final index = Random().nextInt(20);
     print(index);
-    _slotMachineController.start(hitRollItemIndex: index < 5 ? index : null);
+    _slotMachineController.start(hitRollItemIndex: index <= 7 ? index : null);
     context.read<SlotMachineCubit>().setSlotsValue(
           firstSlot: true,
           secondSlot: true,
           thirdSlot: true,
         );
+    context.read<SlotMachineCubit>().setPrizeIndex(index);
   }
 
   void _onSlotMachineStop({required int index}) {
