7b3914bbc78a49fef945cec0f48f8e78990ed34d
Roman Ovsepian <roman.ovsepian@extrawest.com>
2022-12-15 17:50:52 +0200

    Add textstyles

--- lib/theme.dart
+++ lib/theme.dart
@@ -6,5 +6,6 @@ final lightTheme = ThemeData();
 
 class TextStyles {
   static const bodyReg20 = TextStyle(color: primaryWhite, fontSize: 20);
+  static const bodyReg24 = TextStyle(color: primaryWhite, fontSize: 24);
   static const h1Bold45 = TextStyle(color: primaryWhite, fontSize: 45, fontWeight: FontWeight.w700);
 }
