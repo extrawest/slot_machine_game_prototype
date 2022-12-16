dba40292dd2bbdcf8e7d889716444b503e041a89
Roman Ovsepian <roman.ovsepian@extrawest.com>
2022-12-16 17:19:13 +0200

    Add loading indicator to splash_screen.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:slot_machine_game/app.dart';
+import 'package:slot_machine_game/assets.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // Need this asset for splash screen
+  await precachePicture(ExactAssetPicture(SvgPicture.svgStringDecoderBuilder, cherryIc), null);
   runApp(const SlotMachineApp());
 }
--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,5 +1,6 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:lottie/lottie.dart';
 import 'package:slot_machine_game/assets.dart';
@@ -14,9 +15,15 @@ class SplashScreen extends StatefulWidget {
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin {
+  late final AnimationController _animationController;
+
   @override
   void initState() {
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    )..repeat();
     WidgetsBinding.instance.addPostFrameCallback((_) async {
       await _loadComposition();
       _precacheImages(() {
@@ -30,13 +37,35 @@ class _SplashScreenState extends State<SplashScreen> {
   }
 
   @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const GradientBackgroundScaffold(
-      child: Center(
-        child: Text(
-          'Slot Game',
-          style: TextStyles.h1Bold45,
-        ),
+    return GradientBackgroundScaffold(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const Text(
+            'Slot Game',
+            style: TextStyles.h1Bold45,
+          ),
+          const SizedBox(height: 15),
+          AnimatedBuilder(
+            animation: _animationController,
+            builder: (context, child) => Transform(
+              alignment: Alignment.center,
+              transform: Matrix4.identity()
+                ..setEntry(3, 2, 0.001)
+                ..rotateY(_animationController.value * 2 * pi),
+              child: child,
+            ),
+            child: SvgPicture.asset(cherryIc, width: 50),
+          ),
+        ],
       ),
     );
   }
@@ -48,7 +77,6 @@ class _SplashScreenState extends State<SplashScreen> {
     await precachePicture(ExactAssetPicture(SvgPicture.svgStringDecoderBuilder, homeButton), null);
 
     await precachePicture(ExactAssetPicture(SvgPicture.svgStringDecoderBuilder, seventhIc), null);
-    await precachePicture(ExactAssetPicture(SvgPicture.svgStringDecoderBuilder, cherryIc), null);
     await precachePicture(ExactAssetPicture(SvgPicture.svgStringDecoderBuilder, appleIc), null);
     await precachePicture(ExactAssetPicture(SvgPicture.svgStringDecoderBuilder, barIc), null);
     await precachePicture(ExactAssetPicture(SvgPicture.svgStringDecoderBuilder, lemonIc), null);
